2b60b24eb43490b5310cc2ff2f8aedf11ee51583
hyx <hyx2147483647@gmail.com>
2024-10-24 19:52:23 +0800

    update(samd/sam/utils.py): update sam's build method with parameters changed from dataset to batch of tokens.

--- samd/sam/utils.py
+++ samd/sam/utils.py
@@ -1,29 +1,19 @@
 import pickle
 from datasets import Dataset
 from transformers import PreTrainedTokenizerFast
+from typing import List
 
 from .sam import SAM
 from ..samd_config import SamdConfig
 
 def build_sam(
     config: SamdConfig,
-    dataset: Dataset, 
-    tokenizer: PreTrainedTokenizerFast, 
-    column_name: str | None = None
+    batch_tokens: List[List[int]],
+    eos_token: int,
 ):
-    if column_name is None:
-        column_name = dataset.column_names[0]
-    def tokenize_fn(examples):
-        return tokenizer(examples[column_name])
-    tokenized_datasets = dataset.map(
-        tokenize_fn,
-        batched=True,
-        remove_columns=dataset.column_names,
-        desc="Running tokenizer on dataset",
-    )
     sam = SAM.build(
-        tokenized_datasets["input_ids"], 
-        tokenizer.eos_token_id, 
+        batch_tokens, 
+        eos_token, 
         config.n_gram,
         config.k,
     )
