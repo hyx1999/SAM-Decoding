9f4e78e02017c98860891446fa25edcabe9fb589
hyx <hyx2147483647@gmail.com>
2024-11-16 10:11:32 +0800

    update(scripts)

---
+++ scripts/run_ablation_4.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+set -x
+
+cd $(dirname $0)/..
+
+devices=3
+
+CUDA_VISIBLE_DEVICES=${devices} \
+    python -m evaluation.inference_samd \
+    --bench-name spec_bench \
+    --model-path /data/models/vicuna-7b-v1.3 \
+    --model-id vicuna-7b-v1.3-sam_alpaca-abl4 \
+    --sam_path local_cache/sam_none.pkl \
+    --tree_method token_recycle \
+    --samd_n_predicts 15 \
+    --samd_len_threshold 0 \
+    --samd_len_bias 5 \
+    --samd_tree_path default_tree_1_1.json
--- scripts/speed_abl.sh
+++ scripts/speed_abl.sh
@@ -5,4 +5,4 @@ set -x
 cd $(dirname $0)/..
 
 python -m evaluation.speed \
-    --file-path evaluation/data/spec_bench/model_answer/vicuna-7b-v1.3-sam_alpaca-abl3.jsonl
+    --file-path evaluation/data/spec_bench/model_answer/vicuna-7b-v1.3-sam_alpaca-abl4.jsonl
