983bad3b9ab43e1c650d58241b34be5d43646e52
hyx <hyx2147483647@gmail.com>
2024-12-03 19:36:28 +0800

    update(samd_sam_only)

--- evaluation/inference_pia.py
+++ evaluation/inference_pia.py
@@ -3,34 +3,30 @@
 Usage:
 python3 gen_model_answer.py --model-path lmsys/fastchat-t5-3b-v1.0 --model-id fastchat-t5-3b-v1.0
 """
+import torch
 import argparse
 from fastchat.utils import str_to_torch_dtype
-from evaluation.eval import run_evals, reorg_answer_files
-from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedTokenizer
-from samd_sam_only import SamdConfig, SamdModel, SamdGenerationConfig, DraftModel, load_sam
 
-def sam_only_forward(
-    inputs, 
-    model: SamdModel, 
-    tokenizer: PreTrainedTokenizer,
-    max_new_tokens: int, 
-    temperature: float = 0.0,
-    do_sample: bool = False
-):
-    max_cache_len = model.lm.config.max_position_embeddings
-    input_ids = inputs.input_ids
+from evaluation.eval import run_evals, reorg_answer_files
+from evaluation.model.pia.lookahead.common.lookahead_cache import LookaheadCache
+from evaluation.model.pia.lookahead.models.llama.modeling_llama import LlamaForCausalLM
+from functools import partial
+
+def ea_forward(inputs, model, tokenizer, max_new_tokens, temperature=0.0):
     outputs = model.generate(
-        input_ids,
-        generation_config=SamdGenerationConfig(
-            max_new_tokens=max_new_tokens,
-            max_cache_len=max_cache_len,
-        ),
+        input_ids=inputs.input_ids,
+        attention_mask=inputs.attention_mask,
+        max_new_tokens=max_new_tokens,
+        decoding_kwargs={'use_lookahead': True},
+        return_dict_in_generate=True
     )
-    output_ids = outputs.output_ids
-    new_token = outputs.decode_tokens
-    step = outputs.decode_steps
-    accept_length_list = outputs.accepet_length_per_step
-    return output_ids, new_token, step, accept_length_list
+    output_ids = outputs.sequences[0].tolist()
+    kwargs = outputs.kwargs
+    input_ids = torch.tensor([inputs.input_ids[0].tolist() + output_ids], dtype=torch.long)
+    new_token = len(output_ids)
+    step = kwargs["dls"]
+    accept_length_list = [kwargs['edls']] * step
+    return input_ids, new_token, step, accept_length_list
 
 
 if __name__ == "__main__":
@@ -47,12 +43,12 @@ if __name__ == "__main__":
         required=True,
         choices=["vicuna", "llama3"]
     )
+    parser.add_argument("--model-path", type=str, default="/home/lyh/weights/hf/llama2chat/70B/",
+                        help="1")
     parser.add_argument(
-        "--model-path",
-        type=str,
-        required=True,
+        "--load-in-8bit", action="store_false", help="Use 8-bit quantization"
     )
-    parser.add_argument("--model-id", type=str, required=True)
+    parser.add_argument("--model-id", type=str, default="ess-vicuna-70b-fp16")
     parser.add_argument(
         "--bench-name",
         type=str,
@@ -65,15 +61,31 @@ if __name__ == "__main__":
         help="A debug option. The begin index of questions.",
     )
     parser.add_argument(
-        "--question-end",
-        type=int,
-        help="A debug option. The end index of questions."
+        "--question-end", type=int, help="A debug option. The end index of questions."
     )
     parser.add_argument("--answer-file", type=str, help="The output answer file.")
     parser.add_argument(
         "--max-new-tokens",
         type=int,
         default=1024,
+        help="The maximum number of new generated tokens.",
+    )
+    parser.add_argument(
+        "--total-token",
+        type=int,
+        default=60,
+        help="The maximum number of new generated tokens.",
+    )
+    parser.add_argument(
+        "--depth",
+        type=int,
+        default=5,
+        help="The maximum number of new generated tokens.",
+    )
+    parser.add_argument(
+        "--top-k",
+        type=int,
+        default=10,
         help="The maximum number of new generated tokens.",
     )
     parser.add_argument(
@@ -95,7 +107,11 @@ if __name__ == "__main__":
         "--temperature",
         type=float,
         default=0.0,
-        help="The temperature for medusa sampling.",
+    )
+    parser.add_argument(
+        "--tree-choices",
+        type=str,
+        default="mc_sim_7b_63",
     )
     parser.add_argument(
         "--dtype",
@@ -104,27 +120,11 @@ if __name__ == "__main__":
         choices=["float32", "float64", "float16", "bfloat16"],
         help="Override the default dtype. If not set, it will use float16 on GPU.",
     )
-    parser.add_argument(
-        "--sam_path",
-        type=str,
-        default=None
-    )
-    parser.add_argument(
-        "--samd_max_predicts",
-        type=int,
-        default=40
-    )
-    parser.add_argument(
-        "--samd_alpha",
-        type=float,
-        default=4.0
-    )
-    parser.add_argument(
-        "--samd_len_bias",
-        type=int,
-        default=5
-    )
+    parser.add_argument("--is_llama3", action="store_true")
+
     args = parser.parse_args()
+
+    args.model_id = args.model_id + "-temperature-" + str(args.temperature)
 
     question_file = f"evaluation/data/{args.bench_name}/question.jsonl"
 
@@ -134,56 +134,23 @@ if __name__ == "__main__":
         answer_file = f"evaluation/data/{args.bench_name}/model_answer/{args.model_id}.jsonl"
 
     print(f"Output to {answer_file}")
-    
-    print("len_bias:", args.samd_len_bias)
 
-    model = AutoModelForCausalLM.from_pretrained(
+    model = LlamaForCausalLM.from_pretrained(
         args.model_path,
-        torch_dtype=str_to_torch_dtype(args.dtype),
+        torch_dtype=torch.float16,
         low_cpu_mem_usage=True,
-        device_map="auto",
-        # attn_implementation="eager",
+        device_map='auto',
     )
 
-    tokenizer = AutoTokenizer.from_pretrained(args.model_path)
-
-    samd_config = SamdConfig(
-        max_predicts=args.samd_max_predicts,
-        alpha=args.samd_alpha,
-        len_bias=args.samd_len_bias,
-    )
-    if args.sam_path is not None:
-        sam = load_sam(args.sam_path)
-    else:
-        sam = None
-    device = next(model.lm_head.parameters()).device
-    print("device:", device)
-    draft = DraftModel(
-        samd_config,
-        sam_dyn=None,
-        sam_static=sam,
-        lm=model,
-        dtype=str_to_torch_dtype(args.dtype),
-        device=device
-    )
-    samd_model = SamdModel(
-        samd_config, 
-        model, 
-        draft, 
-        tokenizer.eos_token_id,
-        str_to_torch_dtype(args.dtype),
-        device, 
-    )
-
-    if args.temperature > 0:
-        do_sample = True
-    else:
-        do_sample = False
+    tokenizer = model.get_tokenizer()
+    
+    if args.model_type == "llama3":
+        ea_forward = partial(ea_forward, is_llama3=True)
 
     run_evals[args.template](
-        model=samd_model,
+        model=model,
         tokenizer=tokenizer,
-        forward_func=sam_only_forward,
+        forward_func=ea_forward,
         model_id=args.model_id,
         question_file=question_file,
         question_begin=args.question_begin,
@@ -194,7 +161,6 @@ if __name__ == "__main__":
         num_gpus_per_model=args.num_gpus_per_model,
         num_gpus_total=args.num_gpus_total,
         temperature=args.temperature,
-        do_sample=do_sample,
     )
 
     reorg_answer_files[args.template](answer_file)
